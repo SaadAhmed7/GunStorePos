67a91be0838022ff61295022d9a0d44945843be5
SaadAhmed7 <sa3329243@gmail.com>
2025-09-30 00:17:20 +0500

    Configuration Updated

--- src/main/java/com/project/posgunstore/Config/Security/SecurityConfig.java
+++ src/main/java/com/project/posgunstore/Config/Security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",
+                                "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**"
